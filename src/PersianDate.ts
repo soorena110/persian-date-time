5e110b305be0a09096e142e70f5a08aa6a400761
azarang <azarangmr@gmail.com>
2019-05-29 16:23:59 +0430

    toString is added to PersianDate

--- src/PersianDate.ts
+++ src/PersianDate.ts
@@ -13,7 +13,6 @@ export default class PersianDate {
     static monthNames = ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند'];
     static weekNames = ['شنبه', 'یکشنبه', 'دوشنبه', 'سه‌شنبه', 'چهارشنبه', 'پنجشنبه', 'جمعه'];
     static shortWeekNames = ['ش', 'ی', 'د', 'س', 'چ', 'پ', 'ج'];
-
     static gregorianToJalali = gregorianToJalali;
 
     /**
@@ -39,10 +38,19 @@ export default class PersianDate {
     getYear = () => this.getPersianDateObject().year;
     getMonth = () => this.getPersianDateObject().month;
     getDay = () => this.getPersianDateObject().day;
+    valueOf = () => this._date.valueOf();
 
-    valueOf() {
-        return this._date.valueOf();
+    toString(format = 'yyyy/mm/dd') {
+        const date = this.getPersianDateObject();
+
+        const x = format.replace('yyyy', date.year.toString()).replace('yy', (date.year % 100).toString())
+            .replace(/(mm)|(MM)/, (date.month < 10 ? '0' : '') + date.month).replace(/[mM]/, date.month.toString())
+            .replace('dd', (date.day < 10 ? '0' : '') + date.day).replace('d', date.day.toString())
+        return x;
     }
+
+    static Now: PersianDate;
 }
+
 
 Object.defineProperty(PersianDate, 'Now', {get: () => new PersianDate(new Date())});
--- src/_dev/index.ts
+++ src/_dev/index.ts
@@ -1,17 +1,12 @@
 import PersianDate from "../PersianDate";
-import {gregorianToJalali} from "../Convert/gerigorianToJalali";
 
 const p1 = new PersianDate('2019-05-28T04:18:38.699+0000');
-console.log(p1.getPersianDateObject());
+console.log(p1);
 
-
-const p2 = new PersianDate('2019-05-28T04:18:38.699+0000');
+const p2 = PersianDate.Now;
 console.log(p2.getPersianDateObject());
 
 
 const p3 = new PersianDate(1559110235495);
 console.log(p3.getPersianDateObject());
 
-(window as any).PersianDate = PersianDate;
-
-console.log(gregorianToJalali)
